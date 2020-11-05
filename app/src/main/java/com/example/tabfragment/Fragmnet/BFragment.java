da5af7bbe9b8ce62906780cd75576808616e9d49
aldin-nasrun <adnzain40@gmail.com>
2020-11-06 02:31:51 +0700

    add shared view model

---
+++ app/src/main/java/com/example/tabfragment/Fragmnet/BFragment.java
@@ -0,0 +1,54 @@
+package com.example.tabfragment.Fragmnet;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.tabfragment.R;
+import com.example.tabfragment.ViewModel.SharedView;
+
+public class BFragment extends Fragment {
+
+    Button btn_result;
+    EditText et_result;
+    TextView tv_result;
+private SharedView sharedView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_b, container, false);
+        btn_result = v.findViewById(R.id.btn_result);
+        et_result = v.findViewById(R.id.et_result);
+        tv_result = v.findViewById(R.id.tv_result);
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            sharedView.setText(et_result.getText());
+            }
+        });
+        return v;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        sharedView = ViewModelProviders.of(getActivity()).get(SharedView.class);
+        sharedView.getText().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
+            @Override
+            public void onChanged(CharSequence charSequence) {
+                tv_result.setText(charSequence);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/tabfragment/Rest/DbHelp.java
@@ -0,0 +1,4 @@
+package com.example.tabfragment.Rest;
+
+public class DbHelp {
+}
